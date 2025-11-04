570f9f8010d143f21a4dcd8dd5737d5ce699eecf
IciIcifur <iciicifur@yandex.ru>
2025-11-04 14:01:08 +0300

    Gather implementation

--- tasks/pi.c
+++ tasks/pi.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 void naive(int n, int iterations, int rank);
+void manual_collect(int n, int iterations, int rank, int size);
+void gather_collect(int n, int iterations, int rank, int size);
 
 int runTask1() {
     int n, rank, size;
@@ -21,6 +23,8 @@ int runTask1() {
 
 
     naive(n, n / size, rank);
+    manual_collect(n, n / size, rank, size);
+    gather_collect(n, n / size, rank, size);
 
     return 0;
 }
@@ -52,5 +56,75 @@ void naive(int n, int iterations, int rank) {
         printf("PI = %.15f\n\n", pi);
 
         printf("Done in %lfs\n", t1 - t0);
+    }
+}
+
+void manual_collect(int n, int iterations, int rank, int size) {
+    if (rank == 0) printf("\n\nMPI_SEND/MPI_REDUCE  IMPLEMNTATION:\n\n");
+
+    double t0 = MPI_Wtime();
+    int hits = 0;
+    unsigned int seed = time(NULL) + rank;
+
+    srand(seed);
+
+    for (int i = 0; i < iterations; ++i) {
+        double x = ((double)rand()/RAND_MAX) * 2.0 - 1.0;
+        double y = ((double)rand()/RAND_MAX) * 2.0 - 1.0;
+        if (x*x + y*y <= 1.0)
+            ++hits;
+    }
+
+    int total_hits = hits;
+
+    if (rank == 0) {
+        for (int src = 1; src < size; ++src) {
+            int recv_hits;
+            MPI_Recv(&recv_hits, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            total_hits += recv_hits;
+        }
+        double t1 = MPI_Wtime();
+
+        float pi = 4 * (float)total_hits / n;
+        printf("PI = %.15f\n\n", pi);
+        printf("Done in %lfs\n", t1 - t0);
+    } else {
+        MPI_Send(&hits, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+}
+
+void gather_collect(int n, int iterations, int rank, int size) {
+    if (rank == 0) printf("\n\nMPI_GATHER IMPLEMNTATION:\n\n");
+
+    double t0 = MPI_Wtime();
+    int hits = 0;
+    unsigned int seed = time(NULL) + rank;
+    srand(seed);
+
+    for (int i = 0; i < iterations; ++i) {
+        double x = ((double)rand()/RAND_MAX) * 2.0 - 1.0;
+        double y = ((double)rand()/RAND_MAX) * 2.0 - 1.0;
+        if (x*x + y*y <= 1.0)
+            ++hits;
+    }
+
+    int *all_hits = NULL;
+    if (rank == 0) {
+        all_hits = malloc(size * sizeof(int));
+    }
+
+    MPI_Gather(&hits, 1, MPI_INT, all_hits, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    double t1 = MPI_Wtime();
+
+    if (rank == 0) {
+        int total_hits = 0;
+        for (int i = 0; i < size; ++i) {
+            total_hits += all_hits[i];
+        }
+        float pi = 4 * (float)total_hits / n;
+        printf("PI = %.15f\n\n", pi);
+        printf("Done in %lfs\n", t1 - t0);
+        free(all_hits);
     }
 }
