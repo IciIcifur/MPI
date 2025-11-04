3b984d5b3c4bb9638e16896aa2d0b25000357728
IciIcifur <iciicifur@yandex.ru>
2025-11-04 11:29:37 +0300

    Test commit

--- tasks/pi.c
+++ tasks/pi.c
@@ -2,6 +2,10 @@
 #include "tasks.h"
 
 int runTask1() {
-    printf("Running task 1\n");
-    return 1;
+    int n;
+    printf("Enter the iterations number: ");
+    scanf("%d", n);
+    if (n < 0) return 1;
+
+    return 0;
 }
